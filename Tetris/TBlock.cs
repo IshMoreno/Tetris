7578863c26589b348d1cdae16b1cb1790fd4dab5
Ishmael Moreno <77425666+IshmaelMorenoH@users.noreply.github.com>
2025-01-12 22:51:44 -0500

    This completes the TBlock class.

---
+++ Tetris/TBlock.cs
@@ -0,0 +1,24 @@
+﻿namespace Tetris
+{
+    /// <summary>
+    /// This class stores the four position for the <see cref="Block"/> class.
+    /// </summary>
+    public class TBlock : Block
+    {
+        private readonly Position[][] tiles = new Position[][]
+        {
+            new Position[] {new(0,1), new(1,0), new(1,1), new(1,2) },
+            new Position[] {new(0,1), new(1,1), new(1,2), new(2,1) },
+            new Position[] {new(1,0), new(1,1), new(1,2), new(2,1) },
+            new Position[] {new(0,1), new(1,0), new(1,1), new(2,1) }
+        };
+
+        /// <summary>
+        /// This property spawns the block in the middle of the top row and returns the 
+        /// tiles array from above.
+        /// </summary>
+        public override int Id => 6;
+        protected override Position StartOffset => new Position(0, 3);
+        protected override Position[][] Tiles => tiles;
+    }
+}
