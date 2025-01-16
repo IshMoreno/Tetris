438415ecd117f91dd98578aa1f9e03fcaff9835f
Ishmael Moreno <77425666+IshmaelMorenoH@users.noreply.github.com>
2025-01-15 22:38:53 -0500

    Began on the GameState class.

---
+++ Tetris/GameState.cs
@@ -0,0 +1,17 @@
+﻿namespace Tetris
+{
+    public class GameState
+    {
+        private Block currentBlock;
+
+        public Block CurrentBlock
+        {
+            get => currentBlock;
+            private set
+            {
+                currentBlock = value;
+                currentBlock.Reset();
+            }
+        }
+    }
+}
